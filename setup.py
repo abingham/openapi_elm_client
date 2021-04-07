fdacac608418b3576fda2fd28734753751338cc0
Austin Bingham <austin.bingham@gmail.com>
2021-04-07 10:11:34 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openapi_elm_client",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages("src"),
     author="Austin Bingham",
     author_email="austin.bingham@gmail.com",
